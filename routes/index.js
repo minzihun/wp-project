aade592a8b60d68699f807af08d538d909c4d473
honni <a01080704133@gmail.com>
2018-11-26 16:55:53 +0900

    make a comment for routes(self-study)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+//특정 URI/method가 왔을 때 처리하는 handler 등록
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //req: http request, res: http response, next: 다음 middleware 호출
   res.render('index', { title: 'Express' });
 });
 
