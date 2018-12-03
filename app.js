47e6f70b1b895d4b7f42a90bfba3b617465cbced
honni <a01080704133@gmail.com>
2018-12-03 20:12:34 +0900

    plus some modules have to need this proj

--- app.js
+++ app.js
@@ -82,7 +82,9 @@ app.use('/questions', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handleㄴr
