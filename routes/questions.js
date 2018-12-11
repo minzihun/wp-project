40fc84127e7023fffef52da43490e04f3bfd9e4c
honni <a01080704133@gmail.com>
2018-12-11 23:59:23 +0900

    fixed AWS ploblem

--- routes/questions.js
+++ routes/questions.js
@@ -3,6 +3,9 @@ const Question = require('../models/question');
 const Answer = require('../models/answer'); 
 const catchErrors = require('../lib/async-error');
 
+const multer = require('multer');
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = io => {
   const router = express.Router();
@@ -85,7 +88,24 @@ module.exports = io => {
     res.redirect('/questions');
   }));
 
-  router.post('/', needAuth, catchErrors(async (req, res, next) => {
+  const mimetypes = {
+    "image/jpeg": "jpg",
+    "image/gif": "gif",
+    "image/png": "png"
+  };
+  const upload = multer({
+    dest: 'tmp', 
+    fileFilter: (req, file, cb) => {
+      var ext = mimetypes[file.mimetype];
+      if (!ext) {
+        return cb(new Error('Only image files are allowed!'), false);
+      }
+      cb(null, true);
+    }
+  }); // tmp라는 폴더를 미리 만들고 해야 함.
+
+
+  router.post('/', needAuth, upload.single('img'), catchErrors(async (req, res, next) => {
     const user = req.user;
     var question = new Question({
       title: req.body.title,
@@ -100,6 +120,13 @@ module.exports = io => {
       etc: req.body.etc,
       tags: req.body.tags.split(" ").map(e => e.trim()),
     });
+    if (req.file) {
+      const dest = path.join(__dirname, '../public/images/uploads/');  // 옮길 디렉토리
+      console.log("File ->", req.file); // multer의 output이 어떤 형태인지 보자.
+      const filename = question.id + "/" + req.file.originalname;
+      await fs.move(req.file.path, dest + filename);
+      question.img = "/images/uploads/" + filename;
+    }
     await question.save();
     req.flash('success', 'Successfully posted');
     res.redirect('/questions');
